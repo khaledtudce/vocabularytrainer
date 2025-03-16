7902ed38b6e8f3d8146ed966976c0ceca7dcd430
khaled Reza <khaledreza@gmail.com>
2025-03-16 20:46:59 +0100

    updated page.tsx and removed home.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import FlashCard from "@/manualcomponent/FlashCard";
 import Navbar from "@/manualcomponent/NavBar";
+import React, { useState } from "react";
 
-export default function Home() {
+const Home = () => {
+  const [selectedWordIdFrom, setSelectedWordIdFrom] = useState(1);
+  const [selectedWordIdTo, setSelectedWordIdTo] = useState(30);
   return (
     <div className="bg-bgcolor h-screen">
-      <Navbar />
-      <FlashCard />
+      <Navbar
+        onSelectWordIdFrom={setSelectedWordIdFrom}
+        onSelectWordIdTo={setSelectedWordIdTo}
+      />
+      <FlashCard
+        page={"learning"}
+        direction={""}
+        selectedWordIdFrom={selectedWordIdFrom}
+        selectedWordIdTo={selectedWordIdTo}
+      />
     </div>
   );
-}
+};
+
+export default Home;
