49d76487b715c437ae9d1f87c349d7aecb164634
khaled Reza <khaledreza@gmail.com>
2025-03-16 21:11:01 +0100

    small type update for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "standalone",
+  distDir: "build",
 };
 
 export default nextConfig;
