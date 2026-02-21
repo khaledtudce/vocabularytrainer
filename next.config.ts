a168b3458696dc42380704ebfb4507b477bb4102
Khaled Reza <Khaled.Reza@vaberlin.de>
2026-02-21 22:57:37 +0100

    deleted unwanted files

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  distDir: "build",
-  images: { domains: ["encrypted-tbn0.gstatic.com", ""] },
-};
-
-export default nextConfig;
